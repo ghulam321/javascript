aef862098245700256a5c24096ae7609f8e63023
Ghulam Mustafa <108064352+ghulam321@users.noreply.github.com>
2024-07-01 11:13:04 +0000

    datatypes done

---
+++ javascript/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3); //we are using nodejs, not browser
+
+console.log(3+3)// code readability should be high
+
+console.log("ghulam")
+
+
+let name = "ghulam"
+let age = 18;
+let isLoggedIn = false
+
+
+// number => 2 to power 53 or 52
+// bigint
+// string => "" empty string doesn't mean it is null because null itself is a value
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "ghulam");
+console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
